606e76bdded357727e4eda64cfddaeade9e9a075
John Landis <jlandis@bcorporation.net>
2021-02-17 11:17:24 -0500

    fix?

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import TestRoll from '../../components/TestRoll'
 
-export default class BlogIndexPage extends React.Component {
+export default class TestIndexPage extends React.Component {
   render() {
     return (
       <Layout>
